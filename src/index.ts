d138849c390c028100dd57cf6bbc628dbe658596
zraphael <raphael@wcarvalho.com>
2022-06-10 21:06:35 -0300

    feat: updates in our interaction with the contract

--- src/index.ts
+++ src/index.ts
@@ -28,14 +28,31 @@ async function run() {
         throw new Error("Nothing is working in here, damn.");
     }
 
-    const hello = new ethers.Contract(
-        "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+    const counter = new ethers.Contract(
+        process.env.CONTRACT_ADDRESS,
         [
-            "function hello() public pure returns (string memory)",
+            "function count() public",
+            "function getCounter() public view returns (uint32)",
         ],
-        new ethers.providers.Web3Provider(getEth())
+        new ethers.providers.Web3Provider(getEth()).getSigner(),
     );
-    document.body.innerHTML = await hello.hello();
+
+    const el = document.createElement("div");
+    async function setCounter() {
+        el.innerHTML = await counter.getCounter();
+    }
+    setCounter();
+
+    const button = document.createElement("button");
+    button.innerText = "increment";
+    button.onclick = async function() {
+        const tx = await counter.count();
+        await tx.wait();
+        setCounter();
+    }
+
+    document.body.appendChild(el);
+    document.body.appendChild(button);
 }
 
 run();
