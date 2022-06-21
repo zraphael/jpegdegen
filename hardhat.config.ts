396ac1d0635efc5884cd132a5d110dee7eaf0cb9
zraphael <raphael@wcarvalho.com>
2022-06-20 22:43:13 -0300

    adds rinkeby config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,16 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.10",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.RINKEBY_KEY}`],
     }
   }
 };
